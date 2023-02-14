16ef418dad85f8988d4defbd01d05c10142d337d
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-02-14 15:25:45 +0900

    Time: 31 ms (95.77%), Space: 14 MB (24.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -13,10 +13,9 @@ class Solution:
 
         if list1.val <= list2.val:
             list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
         else:
-            list1, list2 = list2, list1
-            list1.next = self.mergeTwoLists(list1.next, list2)
-
-        return list1
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
         
         
