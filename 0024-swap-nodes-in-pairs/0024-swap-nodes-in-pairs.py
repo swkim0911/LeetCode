33cb8c99e7959d567f59797fd2523a93151b8afe
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-02-16 17:02:44 +0900

    Time: 34 ms (61.88%), Space: 13.9 MB (55.85%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,12 +5,18 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur = head
+        root = prev = ListNode(None)
+        prev.next = head
         
-        while cur and cur.next:
-            cur.val, cur.next.val = cur.next.val, cur.val
-            cur = cur.next.next
+        while head and head.next:
+            b = head.next
+            head.next = b.next
+            b.next = head
+            
+            prev.next = b
+            
+            head = head.next
+            prev = prev.next.next
         
-        
-        return head
+        return root.next
         
