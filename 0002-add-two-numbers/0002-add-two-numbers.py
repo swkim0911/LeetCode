dbac5218195286f6ffec8817e2ecb61698adb5ce
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-02-16 15:39:03 +0900

    Time: 65 ms (85.48%), Space: 13.9 MB (35.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,42 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # 연결 리스트 뒤집기
-    def reverseList(self, head: ListNode) -> ListNode:
-        node, prev = head, None
-
-        while node:
-            next, node.next = node.next, prev
-            prev, node = node, next
-
-        return prev
-
-    # 연결 리스트를 파이썬 리스트로 변환:
-    def toList(self, node:ListNode) -> List:
-        list:List = []
-        while node:
-            list.append(node.val)
-            node = node.next
-        return list
-
-    # 결과를 연결 리스트로 변환
-    def toReversedLinkedList(self, result: str):
-        prev: ListNode = None
-        for r in result:
-            node = ListNode(r)
-            node.next = prev
-            prev = node
-        return node
-
     def addTwoNumbers(self, l1: Optional[ListNode],
-                      l2: Optional[ListNode]) -> Optional[ListNode]:
-        a = self.toList(self.reverseList(l1))
-        b = self.toList(self.reverseList(l2))
+                  l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = head = ListNode(0)
 
-        resultStr = int(''.join(str(e) for e in a)) + \
-                    int(''.join(str(e) for e in b))
+        carry = 0  
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
 
-        return self.toReversedLinkedList(str(resultStr))
-        
+            carry, val = divmod(sum+carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+
+        return root.next
         
         
