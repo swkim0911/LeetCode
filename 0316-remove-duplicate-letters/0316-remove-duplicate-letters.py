dd52b2edfe4a29f69296667bf9c3bc9d1c1a964a
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-02-21 15:18:48 +0900

    Time: 41 ms (50.11%), Space: 13.8 MB (74.69%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,14 +1,20 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        counter, seen, stack = collections.Counter(s), set(), []
-        
+        collection = collections.Counter(s) # counter는 for문을 돌면서 앞으로 그 letter가 또 나올 수 있는지 count
+        seen = set()
+        stack = []
+
         for char in s:
-            counter[char] -= 1
+            collection[char] -= 1
+
             if char in seen:
                 continue
-            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+            
+            # char가 stack 맨 위 문자보다 앞에 있고 stack 맨 위 문자가 나중에 더 나올 수 있따면 pop
+            while stack and char < stack[-1] and collection[stack[-1]] > 0:
                 seen.remove(stack.pop())
+
             stack.append(char)
             seen.add(char)
-        
+
         return ''.join(stack)
