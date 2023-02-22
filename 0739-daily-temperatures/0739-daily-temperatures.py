12488f78c56f8da079735ad4b79bba67cc9a83aa
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-02-22 15:49:51 +0900

    Time: 1586 ms (21.01%), Space: 28.5 MB (67.80%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0] * len(temperatures)
-        stack = []
+        stack = [] # stack에 계속 쌓인다는 것은 나보다 더 높은 온도를 만나지 못한것
+        result = [0] * len(temperatures)
+        
         for i, cur in enumerate(temperatures):
+            
+            # 현재 내가 stack의 맨 위 날씨 보다 낮다면 stack에 있는 모든 날씨보다 내가 낮을 수 밖에 없다.
+            # 그래서 스택의 맨 위만 확인해도 된다.
             while stack and cur > temperatures[stack[-1]]:
                 last = stack.pop()
-                answer[last] = i - last
+                result[last] = i - last
             
-            stack.append(i)
+            stack.append(i) # 인덱스를 저장함으로써 인덱스 차이로 몇 일만에 높은 온도가 왔는지 알 수 있다.
         
-        return answer
+        return result
         
