ae935e60756595d703a6428d8604ecd76492aa63
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-03-04 17:28:10 +0900

    Time: 99 ms (91.34%), Space: 18.5 MB (88.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freqs = collections.Counter(nums);
-        freqs_heap = []
-        top_k = []
+        dict = collections.Counter(nums)
+        result = []
 
-        # 빈도 수를 키로 하고, freqs의 key를 값으로 했다.
-        # 파이썬의 힙은 최소 힙을 지원하기 때문에 음수를 붙힘.
-        for key in freqs:
-            heapq.heappush(freqs_heap, (-freqs[key], key))
+        most_frequency = dict.most_common(k)
+        for i, j in most_frequency:
+            result.append(i)
 
-        for _ in range(k):
-            top_k.append(heapq.heappop(freqs_heap)[1]);
-        return top_k
+        return result
+        
+        # freqs = collections.Counter(nums);
+        # freqs_heap = []
+        # top_k = []
+        
+        # # 빈도 수를 키로 하고, freqs의 key를 값으로 했다.
+        # # 파이썬의 힙은 최소 힙을 지원하기 때문에 음수를 붙힘.
+        # for key in freqs:
+        #     heapq.heappush(freqs_heap, (-freqs[key], key))
+        # 
+        # for _ in range(k):
+        #     top_k.append(heapq.heappop(freqs_heap)[1]);
+        # return top_k
