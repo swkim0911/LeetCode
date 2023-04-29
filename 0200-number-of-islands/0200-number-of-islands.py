58b3169ce1cab8a4f4509f61087079bad85c04d9
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-04-29 21:11:42 +0900

    Time: 338 ms (33.37%), Space: 18.8 MB (39.60%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,28 @@
 class Solution:
+    
+    def dfs(self, grid: List[List[str]], i: int, j: int):
+        # 더 이상 땅이 아닌 경우 종료
+        if i < 0 or i >= len(grid) or j < 0 or j >=len(grid[0]) or grid[i][j] != '1':
+            return
+
+        grid[i][j] = "0"  # 거친 땅은 "1"이 아닌 값
+        # 동서남북 탐색
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i, j + 1)
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i, j):
-            # 더 이상 땅이 아닌 경우 종료
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
-                return
-            
-            grid[i][j] = 0
-            
-            #동서남북 탐색
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-        
+
+        if not grid:
+            return 0
+
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    dfs(i,j)
-                    # 모든 육지 탐색 후 카운트 증가
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    # 모든 육지 탐색 후 카운트 1 증가
                     count += 1
-        
-        return count
 
+        return count
         
