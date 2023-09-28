37ddcdbfebc5aa089311b1556ec5f871bbd5cffe
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2023-09-28 18:14:37 +0900

    Time: 58 ms (76.48%), Space: 55.5 MB (58.57%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int last = stack.pop();
+                answer[last] = i - last;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
