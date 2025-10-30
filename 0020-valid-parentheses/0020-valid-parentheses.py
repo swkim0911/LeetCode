111a0fc773f03d1afe73423c56ca85e92e382232
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-10-30 14:06:37 +0900

    Time: 0 ms (100%), Space: 17.8 MB (58.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        table = {
-            ')': '(',
-            '}': '{',
-            ']': '[',
-        }
+        bracket_pair = {"}": "{", ")": "(", "]": "["}
 
-        for char in s:
-            if char not in table:
-                stack.append(char)
-            elif not stack or table[char] != stack.pop():
-                return False
+        for bracket in s:
+            if bracket == "(" or bracket == "{" or bracket == "[": # 열린 괄호인 경우 스택에 push
+                stack.append(bracket)
+            else: # 단힌 괄호인 경우
+                if not stack: # 스택이 비어있는 경우
+                    return False
+                if stack[-1] == bracket_pair[bracket]:
+                    stack.pop()
+                else:
+                    return False
+        
+        if stack:
+            return False
+        
+        return True
+            
 
-        return len(stack) == 0
+# 문제는 주어진 문자열에 대해 괄호쌍이 올바른지 아닌지 확인하는 문제
+# 문자열을 선형으로 탐색하면서 괄호쌍이 올바른지 확인하기 위해서 스택을 사용
