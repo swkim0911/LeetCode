573669d789aaa6412eb0dfc1c8493d1443b8d9a5
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-10-31 13:58:36 +0900

    Time: 0 ms (100%), Space: 17.5 MB (97.37%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,13 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = -1
+        i = 0
         for j in range(0, len(nums)):
             if nums[j] != val:
-                i += 1
                 nums[i] = nums[j]
-        return i + 1
+                i += 1
+        return i
 
-
-# i,j 
-# 둘다 아니면 i+1, j+1
-# i는 아니고, j는 맞으면 j가 아닌거 찾을때까지 앞으로
-# 찾으면 i+1하고 i+1 위치랑 j위치 교환
+# 정의: in-place, 즉 배열을 새로 생성하지 않고 val과 같은 값을 nums 배열에서 삭제해야 한다.
+# 해결책: 새로운 배열을 생성하지 않고, val과 같은 값을 생성하기 위해서는 
+# 배열 안에 val과 다른 값을 저장할 위치를 참고할 수 있는 i, 그리고 linear하게 탐색할 j 변수가 필요하다.
+# nums 배열을 탐색하면서 val과 다른 값
