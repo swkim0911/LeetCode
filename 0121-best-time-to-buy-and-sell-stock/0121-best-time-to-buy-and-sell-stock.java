2872ac14032974fa8bf7bd7352485dad7dc6009e
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-10-31 15:56:50 +0900

    Time: 2 ms (74.13%), Space: 61.9 MB (31.27%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,22 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int minVal = Integer.MAX_VALUE;
+        int minValue = Integer.MAX_VALUE;
         int maxProfit = 0;
 
         for(int price : prices){
-            if(price < minVal){
-                minVal = price;
-            }else if(price - minVal > maxProfit){
-                maxProfit = price - minVal;
+            if(price < minValue){
+                minValue = price;
+            }else if(price - minValue > maxProfit){
+                maxProfit = price - minValue;
             }
         }
 
-        return maxProfit; 
+        return maxProfit;
+
     }
 }
 // 정의: 주식의 가격을 담은 배열이 주어졌을 때, 가장 큰 이윤값을 찾는 문제입니다. 즉, 낮은 가격에 사서 비싼 가격으로 팔 때, 가장 큰 gap을 찾아야 합니다.
 // 풀이: 먼저 가장 쉽게 풀 수 있는 방법은 prices 배열의 모든 원소끼리 비교해서 가장 큰 이윤을 찾는겁니다. 하지만 prices 배열의 총 크기는 10만이고 시간복잡도가 O(N^2)로 100억번 연산해야 해서 시간초과가 발생할 수 있습니다.
 // 그래서 다른 방법을 선택해야 하는데
-
-
-// 
-
+// prices 배열을 순회하는 데, 현재 시점까지 최소 값을 기억해두고, 최소 값과 현재 값의 차이 중에 가장 큰 값을 찾으면 그 값이 멕시멈 profit이 됩니다.
