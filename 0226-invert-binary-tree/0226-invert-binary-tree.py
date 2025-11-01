a547b8acaf0059dcddf9838a32e3dacc431f165d
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-01 21:55:40 +0900

    Time: 0 ms (100%), Space: 17.6 MB (81.08%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -5,29 +5,29 @@
 #         self.left = left
 #         self.right = right
 
-import sys
-
-sys.setrecursionlimit(10**9)
-
 class Solution:
 
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return root
+    # 현재 node에서 invert한 상태를 반환한다.
+    def invertTree(self, node: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not node:
+            return node
         
-        left = self.invertTree(root.left)
-
-        right = self.invertTree(root.right)
+        left = self.invertTree(node.left)
+        right = self.invertTree(node.right)
         
-
-        root.left, root.right = right, left 
-    
-        return root
+        node.left, node.right = right, left 
+        return node
          
+# 문제 정의: 주어진 이진 트리르 좌우 반전해서 root node를 반환합니다.
+# 해결: 재귀를 사용해서 해결합니다. 
+# invertTree 함수를 "현재 node에서 invert한 상태를 반환하는 함수"로 정의 했을 때,
+# 왼쪽 서브트리에 대해 invertTree 함수를 호출해서 반환받은 결과값(inverted left sub-tree)과
+# 오른쪽 서브트리에 대해 invertTree 함수를 호출해서 반환받은 결과값(inverted right sub-tree)을 
+# 현재 노드 기준으로 왼쪽 서브 트리를 그 반환된 오른쪽 서브 트리에 대한 값, 그리고 현재 노드 기준으로 오른쪽 서브 트리를 
+# 반환된 왼쪽 서브 트리에 대한 값을 서로 swapping 한다면
+# 문제에서 요구하는 결과값을 받을 수 있습니다.
 
 
-# left -> right -> parent
-# parent로 왔을 때 left(subtree)와 right(subtree)
 
 
         
