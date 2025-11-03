b506a916f6756f6a2a7c3e8bca60dca97da3c527
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-03 16:43:19 +0900

    Time: 3 ms (1.12%), Space: 18 MB (40.23%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,27 +1,50 @@
 class MyQueue:
 
     def __init__(self):
-        self.input = []
-        self.output = []
-        
+        self.stack1 = []
+        self.stack2 = []
+
+    # stack1은 임시 저장소
+    # stack2는 pop 혹은 peek을 위한 저장소
+    # b c d
+    # c b - peek or pop (연산이 끝나면 stack2는 비워지고 다시 stack1을 채운다)
+
+
+
+
     def push(self, x: int) -> None:
-        self.input.append(x)
-        
+        self.stack1.append(x)
+
     def pop(self) -> int:
-        self.peek()
-        return self.output.pop()
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        
+        head_val = self.stack2.pop()
+        
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+        return head_val
+
 
     def peek(self) -> int:
-        if not self.output:
-            while self.input:
-                self.output.append(self.input.pop())
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
         
-        return self.output[-1]
+        head_val = self.stack2[-1]
+        
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+        return head_val
+        
 
     def empty(self) -> bool:
-        return self.input == [] and self.output == []
-        
-        
+        return not self.stack1
+
+# 문제 정의: 두 개의 스택을 이용해서 큐를 구현한다. 단, 스택은 스택의 표준 연산만 할 수 있다.
+# 풀이: 하나의 stack(a)에는 push되는 데이터의 임시 저장소가 된다. 
+# 그리고 pop이나 peek이 될 때, stack의 가장 먼저 들아건 데이터가 필요하니까 a에서 pop하면서 다른 스택(b)으로 데이터를 push한다. 그러면 a 스택의 가장 뒤에 있는게 b 스택에는 가장 위에 올라오게 되어 First in Frist Out을 구현할 수 있다.
 
 
 # Your MyQueue object will be instantiated and called as such:
