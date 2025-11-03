420a682681dcea5a3f1bd1ce3a23aca376d343f5
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-03 11:01:39 +0900

    Time: 2 ms (70.42%), Space: 18.7 MB (90.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -25,3 +25,8 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         self.get_height(root)
         return self.is_balanced
+
+# 문제 정의: 트리의 모든 노드에서 왼쪽, 오른쪽 서브트리의 높이 차이가 1 이하이면 True, 아니면 False를 반환해야 한다.
+# 풀이: 각 노드들을 방문해서 왼쪽, 오른쪽 서브트리를 높이를 찾기 위해서 재귀를 이용한다.
+# get_height 함수는 재귀로 각 노드의 높이를 구한다. (leaf 노드의 height = 0)
+# 어떤 노드에서 양쪽 서브트리의 높이를 모두 구하고 비교했을 떄, 그 높이 차이가 2 이상이면 전역 인스턴스 변수로 선언한 is_balanced 변수를 False로 처리한다.
