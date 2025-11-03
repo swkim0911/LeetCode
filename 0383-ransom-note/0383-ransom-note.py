21ba200e9dad2385777994adb6882dde6358dddc
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-03 16:54:46 +0900

    Time: 26 ms (26.71%), Space: 18.1 MB (34.35%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -18,4 +18,4 @@ class Solution:
         
         return True
 # 문제 정의: magazine의 문자 집합으로 ransomNote 문자를 구성할 수 있는지 확인하는 문제이다.
-# 해결: dict 자료 구조를 이용해서 각 문자열의 letter가 몇 개인지 Count한다.
+# 해결: dict 자료 구조를 이용해서 magazine 문자열에 letter가 각각 몇 개 있지 count한다. 그리고 ransomNote의 문자를 순회하면서 m_dict에 대응하는 letter 키의 value를 -=1 하며 m_dict에 없거나 <= 0이 되면 magazine은 ransomNote를 만들지 못한다.
