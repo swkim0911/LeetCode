459e2e590bede63ca0a7dc1b6bb9b6b11e40c9ee
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-03 16:53:02 +0900

    Time: 27 ms (24.24%), Space: 17.9 MB (54.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,21 @@
 from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        r_dict = Counter(ransomNote)
-        m_dict = Counter(magazine)
+        m_dict = dict()
+        
+        for letter in magazine:
+            if letter in m_dict:
+                m_dict[letter] += 1
+            else:
+                m_dict[letter] = 1
 
-        for letter, cnt in r_dict.items():
-            if m_dict[letter] < cnt:
+        for letter in ransomNote:
+            if letter not in m_dict:
                 return False
+            if m_dict[letter] <= 0:
+                return False
+            m_dict[letter] -= 1
         
         return True
+# 문제 정의: magazine의 문자 집합으로 ransomNote 문자를 구성할 수 있는지 확인하는 문제이다.
+# 해결: dict 자료 구조를 이용해서 각 문자열의 letter가 몇 개인지 Count한다.
