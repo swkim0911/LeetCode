84ea584601441f5002c51cba6086005c0f862929
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-04 11:41:22 +0900

    Time: 3 ms (49.78%), Space: 19.8 MB (19.36%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -26,4 +26,10 @@ class Solution:
 
 # 브루트포스: 직관적으로 10_001 크기의 배열을 선언하고 interval 구간을 직접 표시한 다음에 거기에 newInterval을 반영할 수 있지만 이러면 10억 번 연산을 해야 해서 timeout이 발생할 수 있다.
 
-# 풀이: 
+# 시간 복잡도: O(n)
+# 공간 복잡도: O(n)
+# 풀이: intervals 배열을 순회하면서 newInterval보다 뒤에 있는 구간은 result에 그대로 추가한다.
+# 이후에 newInterval 구간과 겹치는 interval 구간이 발생하면 newInterval 구간에 겹치는 구간을 반영한다.
+# 이때 겹치는 걸 판단하는 조건은 !(interval.end < newInterval.start) 이 된다.
+# 이렇게 정할 수 있는 이유는 오름차순으로 intervals 배열을 순회해서 앞쪽에 겹치지 않는 조건인 newInterval < interval.start 을 수행했기 때문이다.
+# 겹치는 부분을 처리 끝내고 나머지 newInterval보다 앞에 있는 interval을 result에 그대로 추가하면 된다.
