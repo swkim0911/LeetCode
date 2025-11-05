f087fd7e90f0637309ac7e5ad8046d43adf1a4ab
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-05 14:59:21 +0900

    Time: 8 ms (96.35%), Space: 49 MB (6.05%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,61 +2,52 @@ import java.util.*;
 
 class Solution {
 
-    int[] dx = {-1,0,1,0};
-    int[] dy = {0,-1,0,1};
-    static final int INF = Integer.MAX_VALUE;
+    static final int INF = Integer.MAX_VALUE / 2;
 
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int[][] dist = new int[n][m]; // 0에서 최소 거리
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dist[i], INF); // INF으로 초기화
-        }
-        
-        Queue<Point> q = new LinkedList<>();
+        int[][] dp = new int[n][m]; // 현재 위치(1)에서 0까지 거리 중 최소 거리
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(mat[i][j] == 0){
-                    q.add(new Point(i,j));
-                    dist[i][j] = 0;
+                if(mat[i][j] != 0){
+                    dp[i][j] = INF;
                 }
             }
         }
-        while(!q.isEmpty()){
-            Point cur = q.poll();
-
-            for(int i = 0; i < 4; i++){
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if(dist[nx][ny] > dist[cur.x][cur.y] + 1){
-                    dist[nx][ny] = dist[cur.x][cur.y] + 1;
-                    q.add(new Point(nx,ny));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(dp[i][j] > 0){
+                    if(i > 0) dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + 1);
+                    if(j > 0) dp[i][j] = Math.min(dp[i][j], dp[i][j-1] + 1);
+                }
+            }
+        }
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+                if(dp[i][j] > 0){
+                    if(i < n - 1) dp[i][j] = Math.min(dp[i][j], dp[i+1][j] + 1);
+                    if(j < m - 1) dp[i][j] = Math.min(dp[i][j], dp[i][j+1] + 1);
                 }
             }
         }
 
-        return dist;
+        return dp;
     }
 
 }
 
-class Point{
-    int x,y;
 
-    public Point(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
 // 문제 정의: 각 셀에 대해서 가장 가까운 0과의 거리 찾기
-// 해결: 0 에게 가장 가까운 건 자기자신이니까 0.
-// bfs로 0이 아닌 cell에서 bfs로 가장 가까이에 있는 0의 거리를 찾는다.
-// bfs 특성상 큐를 사용하기 때문에 순회를 하면서 가장 먼저 찾은 0일 때, 그때가 최소 거리이다.
-// mat 배열에 0이 아닌 값은 모두 -1로 초기화한 다음에 -1은 아직 최소 거리를 찾지 않는 셀, 정수 > 0는 이미 찾은 셀로 구분한다.
+// 해결: 
+// 처음에는 bfs로 0이 아닌 cell에서 bfs로 가장 가까이에 있는 0의 거리를 찾는다.
+// 이렇게 하면 timeout이 발생한다.
 // 시간 복잡도: O((n*m)*(n*m)) -> timeout
-// 모든 셀을 방문해서 조회하면 timeout이 발생하니까 dp로 해결할 수 있다.
+// 그래서 DP로 이전 경우를 기록한다면 시간복잡도를 줄일 수 있다.
+// dp[x][y]: (x,y) 위치에서 0까지 최단 거리라고 정의하면 점화식은
+// dp[x][y] = Math.min(dp[x][y], dp[x-1][y], dp[x][y-1], dp[x+1][y], dp[x][y+1]) 로 정의할 수 있다.
+// 이때, 이 점화식은 한 번에 계산할 수 없는데 이유는 dp[x-1][y], dp[x][y-1]은 왼쪽 위에서 내려오면서 최단 거리를 갱신하고, dp[x+1][y], dp[x][y+1]은 오른쪽 아래에서 올라가면서 최단 거리가 갱신되기 떄문에, 따로 나누어서 최단 거리를 갱신해야 한다.
 
-// dp의 정수 값만으로 
+// 시간 복잡도: O(n*m)
+// 공간 복잡도: O(n*m)
