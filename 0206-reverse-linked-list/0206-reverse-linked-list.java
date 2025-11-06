f2361fd180d4651231ff8168885b746befc8deb8
swkim0911 <102792381+swkim0911@users.noreply.github.com>
2025-11-06 12:01:20 +0900

    Time: 0 ms (100%), Space: 44.4 MB (5.37%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,22 +9,22 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-
-        while(head != null){
-            ListNode nxt = head.next;
-            head.next = prev;
-
-            prev = head;
-            head = nxt;
+    public ListNode recursion(ListNode prev, ListNode cur){
+        if(cur == null){
+            return prev; // 종료 조건
         }
 
-        return prev;
+        ListNode next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        return recursion(prev, next);
+    }
 
-
-
+    public ListNode reverseList(ListNode head) {
+        return recursion(null, head);
     }
 }
 // 문제 정의: 단일 연결 리스트의 헤드가 주어졌을 때, 뒤집힌 리스트를 반환한다.
-// 문제 해결: 각 노드를 순회하면서 이전 노드를 기억해서, 현재 노드가 이전 노드를 가리킨다.
+// 문제 해결: 각 노드를 순회하는데 이전 노드를 기억해서, 현재 노드가 이전 노드를 가리킨다.
+// 단, 현재 노드에서는 이전 노드를 알 수 없기 떄문에 현재 노드를 보기 전에 미리 이전 노드를 기억해야 한다.
+
